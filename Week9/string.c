182a1e1a9a9f25551c943658afc9635b55fcf97f
20242112 <jyj050928@naver.com>
2024-11-01 12:19:30 +0900

    week9

---
+++ Week9/string.c
@@ -0,0 +1,24 @@
+//string.c
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char hello[6] = { 'H','e','l','l','o',0 };
+	char world[6] = { 'W','o','r','l','d',0 };
+	char ment[] = "C is fun!~";
+
+	printf("%s %s\n%s", hello, world, ment);
+
+	//문자열 Don't worry,be happy! 만들고
+	//문자열 걱정마. 행복할거야. 만듥고
+	//출력해 주세요.
+
+	char worry[] = "Don't worry,be happy!";
+	char worry_ko[] = "걱정마. 행복할 거야.";
+
+	printf("\n\n%s\n%s", worry,worry_ko);
+
+
+	getch();
+	return 0;
+}
